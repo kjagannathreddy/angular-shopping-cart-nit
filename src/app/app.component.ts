390d762880ba84f0aa6da483c3995d5a7847d394
kjagannathreddy <jagannath.bond007@gmail.com>
2019-07-15 15:05:06 +0530

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ export class AppComponent  {
   name = 'Angular';
   public cart;
   public products = [
-    {Name:'Tv',Price:'15000',Photo:'assets/1.jpg'},
-    {Name:'Mobile',Price:'4000',Photo:'assets/1.jpg'},
-    {Name:'Fridge',Price:'14000',Photo:'assets/1.jpg'},
-    {Name:'Washing Machine',Price:'18000',Photo:'assets/1.jpg'},
-    {Name:'Sparx shoes',Price:'1000',Photo:'assets/1.jpg'},
-    {Name:'Killer Jeans',Price:'1500',Photo:'assets/1.jpg'}
+    {Name:'Tv',Price:'15000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
+    {Name:'Mobile',Price:'4000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
+    {Name:'Fridge',Price:'14000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
+    {Name:'Washing Machine',Price:'18000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
+    {Name:'Sparx shoes',Price:'1000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
+    {Name:'Killer Jeans',Price:'1500',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'}
   ];
   public add_to_cart(index){
-    
+    alert(index);
   }
 }
